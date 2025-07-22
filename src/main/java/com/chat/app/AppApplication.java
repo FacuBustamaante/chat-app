09eb0664ce96232227d1a8816133eca07d653b86
FacuBustamaante <facundombustamante@gmail.com>
2025-07-22 11:18:52 -0300

    PostgreSQL database configured.

--- src/main/java/com/chat/app/AppApplication.java
+++ src/main/java/com/chat/app/AppApplication.java
@@ -1,5 +1,6 @@
 package com.chat.app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		SpringApplication.run(AppApplication.class, args);
 	}
-
 }
